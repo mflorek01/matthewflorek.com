ba2b923acda186dd85e9be059c07e3f043e3c92c
mflorek01 <matthew.florek01@gmail.com>
2025-02-08 11:14:59 -0800

    Update data.ts
    updated with skills.expand and added example blog post

--- client/src/lib/data.ts
+++ client/src/lib/data.ts
@@ -1,11 +1,13 @@
 export const skills = [
-  { name: "Microsoft Excel", level: 95 },
-  { name: "Financial Modeling", level: 80 },
-  { name: "Data Analysis", level: 90 },
-  { name: "Financial Literacy", level: 85 },
-  { name: "Tableau", level: 75 },
+  { name: "Microsoft Excel", level: 95, expand: "I possess expert proficiency in Microsoft Excel, and am skilled using large databases, pivot tables, data transformation, and complex lookup formulas." },
+  { name: "Financial Modeling", level: 80, expand: "From my experience at NAU, I have learned extensively how to make financial forecasts and create models." },
+  { name: "Data Analysis", level: 90, expand: "From my internship experience and Business Analytics coursework, I have learned how to analyze data, and use SQL and R to derive useful business insight." },
+  { name: "Financial Literacy", level: 85, expand: "My advanced Finance coursework at NAU has given me a robust foundation in financial concepts, project financing, and financial evaluation techniques." },
+  { name: "Tableau", level: 75, expand: "Through my Business Analytics education, I have become an adept user of Tableau to create reports and dashboards." },
+  { name: "RapidMiner", level: 60, expand: "From my educational experience, I have learned how to effectively use RapidMiner for data mining and data science purposes." },
+  { name: "SQL", level: 50, expand: "I learned how to use SQL to query databases from my UC Davis certification and my time at NAU. These experiences have given me a solid foundation that makes me comfortable in any role requiring SQL expertise." },
 ] as const;
-
+// Another value that exists is skills.desc - creates a secondary line below the title of the skill
 export const education = [
   { degree: "B.S. in Finance", institution: "Northern Arizona University", year: "2024" },
   { degree: "Certificate in Business Analytics", institution: "Northern Arizona University", year: "2024" },
@@ -21,5 +23,12 @@ export const experience = [
 ] as const;
 
 export const blogPosts = [
-
+  {
+    title: "Example Post",
+    description: "My approach to analyzing complex datasets and deriving meaningful insights",
+    image: "https://images.unsplash.com/photo-1432888498266-38ffec3eaf0a",
+    content: `
+      This is an example post to demonstrate the blog post feature.
+    `
+  },
 ] as const;
